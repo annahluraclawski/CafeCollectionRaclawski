f679db00e1e759fd135d8b7169aac97da02b2299
Annahlu Raclawski <annahlu47@gmail.com>
2023-09-21 09:43:20 -0500

    In class September 21

--- CafeCollectionRaclawski/ViewController.swift
+++ CafeCollectionRaclawski/ViewController.swift
@@ -18,8 +18,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var food : [String] = ["Fettucine Alfredo", "Double Cheeseburger", "Orange Chicken", "Pepperoni Pizza", "Turkey Sub"]
-        var prices : [Double] = [14.99, 12.99, 11.99, 16.99, 10.99]
+        var food : [String] = ["Fettucine Alfredo", "Cheeseburger", "Orange Chicken", "Pepperoni Pizza", "Turkey Sub", "Loaded Fries", "Caesar Salad"]
+        var prices : [Double] = [14.99, 12.99, 11.99, 16.99, 10.99, 7.99, 10.99]
         var menu = ""
         
         for (foo, pric) in zip(food, prices) {
