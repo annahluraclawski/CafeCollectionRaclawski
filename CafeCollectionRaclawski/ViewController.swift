e3d4400eb400292951c475082e0e5cb0d782dbeb
Annahlu Raclawski <annahlu47@gmail.com>
2023-09-21 09:19:29 -0500

    Initial

--- CafeCollectionRaclawski/ViewController.swift
+++ CafeCollectionRaclawski/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var menuOutlet: UILabel!
+    
+    @IBOutlet weak var cartOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var food : [String] = ["Fettucine Alfredo", "Double Cheeseburger", "Orange Chicken", "Pepperoni Pizza", "Turkey Sub"]
+        var prices : [Double] = [14.99, 12.99, 11.99, 16.99, 10.99]
+        var menu = ""
+        
+        for (foo, pric) in zip(food, prices) {
+            menu = "\(menu) \(foo): \(pric)\n"
+            
+        }
+        menuOutlet.text = "\(menu)"
+        
     }
-
-
+    
 }
 
